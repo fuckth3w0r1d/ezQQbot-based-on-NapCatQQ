ea4239e5ea8fafdf43e6469dd76955cae82ba150
r3t2 <u16978199gou@163.com>
2026-02-15 15:45:42 +0800

    feat: add AI conversation feature

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 基于 NapCatQQ 搭建的 qq bot<br>
-编译命令 g++ main.cpp -o bot.exe -Iinclude -lws2_32 -lpthread
+编译命令 g++ main.cpp -o bot.exe -Iinclude -IC:/msys64/mingw64/include -LC:/msys64/mingw64/lib -lssl -lcrypto -lcrypt32 -lws2_32 -lpthread
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
 #include "json.hpp"
 
 using json = nlohmann::json;
 
-// config
+//////////////////////// config
+// naocatqq
 std::string BOT_QQ;
 std::string SERVER_HOST;
 int SERVER_PORT;
@@ -12,11 +14,19 @@ std::string SERVER_ACCESS_TOKEN;
 std::string CLIENT_HOST;
 int CLIENT_PORT;
 std::string CLIENT_ACCESS_TOKEN;
-
+// amap
 std::string AMAP_KEY;
 std::string AMAP_CLIENT_HOST;
 int AMAP_CLIENT_PORT;
 std::string AMAP_GET_PATH;
+// ai
+std::string AI_KEY;
+std::string AI_CLIENT_HOST;
+int AI_CLIENT_PORT;
+std::string AI_POST_PATH;
+std::string AI_MODEL;
+std::string AI_SYS_PROMPTS;
+int AI_MAX_TOKENS;
 
 // 用于获取 config 信息
 class Config{
@@ -88,11 +98,47 @@ public:
     {
         return data["amap_api"]["path"].get<const std::string>();
     }
+
+    const std::string getAIKey()
+    {
+        return data["ai_api"]["key"].get<const std::string>();
+    }
+
+    const std::string getAIHost()
+    {
+        return data["ai_api"]["host"].get<const std::string>();
+    }
+
+    const int getAIPort()
+    {
+        return data["ai_api"]["port"].get<const int>();
+    }
+
+    const std::string getAIPostPath()
+    {
+        return data["ai_api"]["path"].get<const std::string>();
+    }
+
+    const std::string getAIModel()
+    {
+        return data["ai_api"]["model"].get<const std::string>();
+    }
+
+    const std::string getAISysPrompts()
+    {
+        return data["ai_api"]["system_prompts"].get<const std::string>();
+    }
+
+    const int getAIMaxTokens()
+    {
+        return data["ai_api"]["max_tokens"].get<const int>();
+    }
 };
 
 void getAllConfigVal()
 {
     Config cfg;
+    // napcatqq
     BOT_QQ = cfg.getBotqq();
     SERVER_HOST = cfg.getServerHost();
     SERVER_PORT = cfg.getServerPort();
@@ -100,10 +146,19 @@ void getAllConfigVal()
     CLIENT_HOST = cfg.getClientHost();
     CLIENT_PORT = cfg.getClientPort();
     CLIENT_ACCESS_TOKEN = cfg.getClientToken();
+    // amap
     AMAP_CLIENT_HOST = cfg.getAmapHost();
     AMAP_CLIENT_PORT = cfg.getAmapPort();
     AMAP_KEY = cfg.getAmapKey();
     AMAP_GET_PATH = cfg.getAmapGetPath();
+    // ai
+    AI_CLIENT_HOST = cfg.getAIHost();
+    AI_CLIENT_PORT = cfg.getAIPort();
+    AI_KEY = cfg.getAIKey();
+    AI_POST_PATH = cfg.getAIPostPath();
+    AI_MODEL = cfg.getAIModel();
+    AI_SYS_PROMPTS = cfg.getAISysPrompts();
+    AI_MAX_TOKENS = cfg.getAIMaxTokens();
 }
 
 // 消息结构
@@ -173,7 +228,7 @@ public:
             {
                 msgctx.raw_msg.erase(0, 1);
             }
-            if(msgctx.raw_msg.empty()) return "指令格式: @我 指令\n先试试 help 吧";
+            if(msgctx.raw_msg.empty()) return "指令格式: @我 指令\n先试试 帮助 吧";
             // 执行指令
             // 先按照空格分割指令名称和参数
             pos = msgctx.raw_msg.find(' ');
@@ -370,7 +425,7 @@ public:
             json raw_data = json::parse(res->body);
             if(raw_data["lives"].empty())
             {
-                return "天气查询失败, 请输入正确的格式: 天气 城市名称\n城市名称不要带有除空格外的其他字符\n暂时只支持国内城市";
+                return "天气查询失败, 请输入正确的格式: 天气 城市名称\n暂时只支持国内城市";
             }
             auto data = raw_data["lives"][0];
             std::string result = "城市: " + data["province"].get<std::string>() + " "
@@ -392,6 +447,117 @@ public:
     }
 };
 
+// AI对话
+class AICommand : public Command{
+private:
+    std::string askAI(std::string& args)
+    {
+        httplib::SSLClient cli(AI_CLIENT_HOST, AI_CLIENT_PORT);
+        json body;
+        httplib::Headers headers = {
+            {"Authorization", "Bearer " + AI_KEY},
+            {"Content-Type", "application/json"}
+        };
+        body["model"] = AI_MODEL;
+        body["messages"] = json::array({
+            {
+                {"role", "system"},
+                {"content", AI_SYS_PROMPTS}
+            },
+            {
+                {"role", "user"},
+                {"content", args}
+            }
+        });
+        body["max_tokens"] = AI_MAX_TOKENS;
+        auto res = cli.Post(AI_POST_PATH, headers, body.dump(), "application/json");
+        // 硅基流动 post 请求示例
+        //         curl --request POST \
+        //   --url https://api.siliconflow.cn/v1/chat/completions \
+        //   -H "Content-Type: application/json" \
+        //   -H "Authorization: Bearer YOUR_API_KEY" \
+        //   -d '{
+        //     "model": "Pro/zai-org/GLM-4.7",
+        //     "messages": [
+        //       {"role": "system", "content": "你是一个有用的助手"},
+        //       {"role": "user", "content": "你好，请介绍一下你自己"}
+        //     ]
+        //   }'
+        if(!res)
+        {
+            return "网络请求失败";
+        }else{
+            std::cout << "HTTP状态码: " << res->status << std::endl;
+            if(res->status == 200)
+            {
+                json result = json::parse(res->body);
+                // 硅基流动api reponse 结构示例
+                // {
+                //     "id": "019bdaa55225ef854b320e9b838f77ce",
+                //     "object": "chat.completion",
+                //     "created": 1768899826,
+                //     "model": "Pro/zai-org/GLM-4.7",
+                //     "choices": [
+                //         {
+                //         "index": 0,
+                //         "message": {
+                //             "role": "assistant",
+                //             "content": "你好！...",
+                //             "reasoning_content": "..."
+                //         },
+                //         "finish_reason": "stop"
+                //         }
+                //     ],
+                //     "usage": {
+                //         "prompt_tokens": 15,
+                //         "completion_tokens": 1540,
+                //         "total_tokens": 1555,
+                //         "completion_tokens_details": {
+                //         "reasoning_tokens": 1190
+                //         },
+                //         "prompt_tokens_details": {
+                //         "cached_tokens": 0
+                //         },
+                //         "prompt_cache_hit_tokens": 0,
+                //         "prompt_cache_miss_tokens": 15
+                //     },
+                //     "system_fingerprint": ""
+                // }
+                if (result.contains("choices") && !result["choices"].empty())
+                {
+                    auto& choices = result["choices"][0];
+                    auto& usage = result["usage"];
+                    std::string ai_reply = choices["message"]["content"].get<std::string>();
+                    std::string total_tokens = std::to_string(usage["total_tokens"].get<int>());
+                    std::string reply = "模型: " + result["model"].get<std::string>() 
+                        + "\nAI回复内容: " + ai_reply + "\n使用 token: " + total_tokens;
+                    return reply;
+                }
+                return "AI 回复异常";
+            }
+            std::cerr << json::parse(res->body).dump() << std::endl;
+            return "AI 请求异常";
+        }
+    }
+public:
+    std::string name() override
+    {
+        return "AI对话";
+    }
+
+    std::string execute(std::string& args) override
+    {
+        if(args.empty())
+        {
+            return "请输入对话内容, 例如 AI对话 对话内容";
+        }
+        return askAI(args);
+    }
+    ~AICommand() override
+    {
+        return;
+    }
+};
 
 int main()
 {
@@ -399,8 +565,8 @@ int main()
     MessageManager m;
     m.cmd_manager.registerCommand(std::make_unique<TimeCommand>());
     m.cmd_manager.registerCommand(std::make_unique<WeatherCommand>());
+    m.cmd_manager.registerCommand(std::make_unique<AICommand>());
     m.cmd_manager.registerCommand(std::make_unique<HelpCommand>(m.cmd_manager.getCommandList()));
     m.start();
     return 0;
 }
-// g++ main.cpp -o bot.exe -Iinclude -lws2_32 -lpthread
